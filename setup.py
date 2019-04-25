d676f7a295b7f9695456d2b27a613732f0419081
Vladik <qvviko@gmail.com>
2019-04-25 18:05:55 +0300

    + setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='logging_transport_json',
+      version='0.1',
+      description='Logging to tcp and udp in json format',
+      url='https://github.com/qvviko/logging_transport_json',
+      author='Vladislav Savchuk',
+      author_email='qvviko@gmail.com',
+      license='MIT',
+      packages=['logging_transport_json'],
+      zip_safe=False)
